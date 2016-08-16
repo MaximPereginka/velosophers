954aed04a1dc2e0d43a6e9aa7dae1f66098a7928
MaximPereginka <maxim.pereginka@outlook.com>
2016-08-16 23:29:56 +0300

    started creating design

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,106 +5,92 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Velosophers</title>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
-
-    <!-- Styles -->
-    <link rel="stylesheet" href="/bootstrap/dist/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-
-    <link rel="stylesheet" href="/admin_style.css" type="text/css">
+    <link rel="stylesheet" href="/bootstrap-main/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="/style.css" type="text/css">
 </head>
 <body id="app-layout">
-<nav class="navbar navbar-default navbar-static-top">
-    <div class="container">
-        <div class="navbar-header">
-
-            <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-
-            <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/dashboard') }}">
-                Velosophers
-            </a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <ul class="nav navbar-nav">
-            @if(!Auth::guest())
-                <!-- Author navbar -->
-                @if(Auth::user()->user_type == 2)
-                    @include('helpers.dashboard.author.navbar')
-                    <!-- Moderator navbar -->
-                    @elseif(Auth::user()->user_type == 3)
-                        @include('helpers.dashboard.moderator.navbar')
-                    @elseif(Auth::user()->user_type == 4)
-                        @include('helpers.dashboard.administrator.navbar')
-                    @endif
-                @endif
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Вход</a></li>
-                    <li><a href="{{ url('/register') }}">Регистрация</a></li>
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
+    <!-- Headline -->
+    <nav class="row headline shadow">
+        <div class="container">
+            <div class="col-lg-3">
+                <div class="row">
+                    <img class="logo" src="/img/logo.png" title="Velosophers"/>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="row">
+                    <nav class="navigation">
+                        <ul>
                             <li>
-                                <a href="/dashboard/private_office">
-                                    <i class="fa fa-btn fa-home"></i>Личный кабинет
-                                </a>
+                                <a href="#">Главная</a>
                             </li>
-                            @if(in_array(Auth::user()->user_type, [2,4]))
-                                <li>
-                                    <a href="{{ (Auth::user()->user_type == 2) ? "/dashboard/author/articles/own" : "/dashboard/administrator/articles/own" }}">
-                                        <i class="fa fa-btn fa-file-image-o"></i>Мои публикации
-                                    </a>
-                                </li>
-                            @endif
                             <li>
-                                <a href="{{ url('/logout') }}">
-                                    <i class="fa fa-btn fa-sign-out"></i>Выйти
-                                </a>
+                                <a href="#">Популярное</a>
+                            </li>
+                            <li>
+                                <a href="#">Категории</a>
+                            </li>
+                            <li>
+                                <a href="#">О проекте</a>
                             </li>
                         </ul>
-                    </li>
-                @endif
-            </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="row">
+                    <div class="login">
+                        <a href="#">Войти</a>
+                        <a class="have-border" href="#">Регистрация</a>
+                    </div>
+                </div>
+            </div>
         </div>
+    </nav>
+    <!-- END Headline -->
+
+    <div class="container">
+        <!-- Content -->
+        <div class="content">
+            <!-- Slider -->
+            <div class="row slider">
+                <img class="shadow" src="http://www.orangesmile.com/ru/foto/usa/fodder120119.jpg" title="Слайдер"/>
+                <div class="description">
+                    <h2>Lorem Ipsum</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, iusto maxime minus nisi porro quam recusandae. Aspernatur cupiditate, delectus ex fugiat magni modi necessitatibus porro praesentium...</p>
+                </div>
+            </div>
+            <!-- END Slider -->
+
+            <div class="row">
+                <div class="col-md-9">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img class="shadow card" src="http://cs626329.vk.me/v626329641/810f/I-y1RvzztX8.jpg" title="Статья"/>
+                        </div>
+                        <div class="col-md-4">
+                            <img class="shadow card" src="http://cs626329.vk.me/v626329641/810f/I-y1RvzztX8.jpg" title="Статья"/>
+                        </div>
+                        <div class="col-md-4">
+                            <img class="shadow card" src="http://cs626329.vk.me/v626329641/810f/I-y1RvzztX8.jpg" title="Статья"/>
+                        </div>
+                        <div class="col-md-4">
+                            <img class="shadow card" src="http://cs626329.vk.me/v626329641/810f/I-y1RvzztX8.jpg" title="Статья"/>
+                        </div>
+                        <div class="col-md-4">
+                            <img class="shadow card" src="http://cs626329.vk.me/v626329641/810f/I-y1RvzztX8.jpg" title="Статья"/>
+                        </div>
+                        <div class="col-md-4">
+                            <img class="shadow card" src="http://cs626329.vk.me/v626329641/810f/I-y1RvzztX8.jpg" title="Статья"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END Content -->
     </div>
-</nav>
 
-@if(Session::has('flash_message_text'))
-    @include('helpers.dashboard.mutual.flash_messages')
-@endif
-
-@if(count($errors))
-    @include('helpers.dashboard.mutual.errors')
-@endif
-
-<div class="container">
-    @yield('content')
-</div>
-
-<!-- JavaScripts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-<script src="/scripts.js"></script>
-{{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>
